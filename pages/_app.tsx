f75281799a9992d7e9521ba3cb7a3d2187da25dd
nthieu98 <nthieu1098@gmail.com>
2022-11-11 09:36:45 +0700

    set font roboto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Inter,Roboto } from '@next/font/google'
+
+// const inter = Inter()
+const roboto = Roboto({
+  weight: '400',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+        <main className={roboto.className}>
+          <Component {...pageProps} />
+        </main>)
 }
