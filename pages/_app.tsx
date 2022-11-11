9d092b02d3e8b6b146197f0b335ae362da53f407
nguyenhoangsangvn <nguyenhoagsang@gmail.com>
2022-11-12 01:20:34 +0700

    Update Font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Roboto, Inter} from '@next/font/google'
 
-const inter = Inter({
+const roboto = Roboto({
 weight: '500',
 })
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
-        <main className={inter.className}>
+        <main className={roboto.className}>
           <Component {...pageProps} />
         </main>
         )
