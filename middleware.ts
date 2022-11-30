da5dcddc7ca7749d045121321f02716e75171932
nguyenhoangsangvn <nguyenhoagsang@gmail.com>
2022-11-30 20:44:16 +0700

    aâ

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/tu-van')) {
-    return NextResponse.rewrite(new URL('https://shorten.asia/bkT8ggUr'))    
+    return NextResponse.rewrite(new URL('https://trackmobi.asia/7qZNtPkh'))    
   }
 
   if (request.nextUrl.pathname.startsWith('/dtd')) {
