5519e6078e4b1419673e03422a0eb284ac0a7a91
nguyenhoangsangvn <nguyenhoagsang@gmail.com>
2022-11-30 21:39:46 +0700

    clone3

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/tu-van')) {
-    return NextResponse.rewrite(new URL('https://mb.mfast.vn?saleCode=510863'))    
+    return NextResponse.rewrite(new URL('https://mfast.vn/cake/510863'))    
   }
 
   if (request.nextUrl.pathname.startsWith('/dtd')) {
