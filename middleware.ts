a258284ad861e2a6082904ae26005f99766202de
nguyenhoangsangvn <nguyenhoagsang@gmail.com>
2022-11-30 13:26:35 +0700

    ahihi

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/tu-van')) {
-    return NextResponse.rewrite(new URL('https://m.me/108405248704761'))    
+    return NextResponse.rewrite(new URL('https://shorten.asia/bkT8ggUr'))    
   }
 
   if (request.nextUrl.pathname.startsWith('/dtd')) {
