c73c8fa6068b392cb7059c18439ab8a23de5bdff
nguyenhoangsangvn <nguyenhoagsang@gmail.com>
2022-12-10 22:10:13 +0700

    appvay

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,54 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/tu-van')) {
-    return NextResponse.rewrite(new URL('https://m.me/100644866097188?ref=Welcome'))    
+    return NextResponse.rewrite(new URL('https://shorten.asia/k8WU65JR'))    
   }
 
-  if (request.nextUrl.pathname.startsWith('/dtd')) {
-    return NextResponse.rewrite(new URL('https://google.com'))    
+  if (request.nextUrl.pathname.startsWith('/cre')) {
+    return NextResponse.rewrite(new URL('https://shorten.asia/bkT8ggUr'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/sen')) {
+    return NextResponse.rewrite(new URL('https://shorten.asia/FGkQtTdv'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/rob')) {
+    return NextResponse.rewrite(new URL('https://shorten.asia/3XaAt7D3'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/tie')) {
+    return NextResponse.rewrite(new URL('https://shorten.asia/cn62Wj3R'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/tam')) {
+    return NextResponse.rewrite(new URL('https://shorten.asia/TK9P1DfQ'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/ava')) {
+    return NextResponse.rewrite(new URL('https://shorten.asia/3hcseg7c'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/jef')) {
+    return NextResponse.rewrite(new URL('https://shorten.asia/DQ2p496d'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/fin')) {
+    return NextResponse.rewrite(new URL('https://shorten.asia/fYDUxmxc'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/fin')) {
+    return NextResponse.rewrite(new URL('https://shorten.asia/fYDUxmxc'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/tnex')) {
+    return NextResponse.rewrite(new URL('https://mfast.vn/tnex/510863'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/cake')) {
+    return NextResponse.rewrite(new URL('https://mfast.vn/cake/510863'))    
+  }
+
+  if (request.nextUrl.pathname.startsWith('/mb')) {
+    return NextResponse.rewrite(new URL('https://mfast.vn/cai-dat/510863'))    
   }
 }
